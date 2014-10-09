a2956b6fac9976bc07113a6055f6b624dcdd58fc
plombeur <hougmickael@gmail.com>
2014-10-09 19:57:57 +0200

    System d'action ajouté + gitignore
    Actions et liste d'actions

---
+++ Assets/Classes/Actions/Action.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+public abstract class Action
+{
+    private string name;
+    private ActionPendingList pendingList;
+    private bool isInit;
+    private bool isEnabled;
+
+    public Action(string name)
+    {
+        this.name = name;
+        isEnabled = true;
+    }
+    public string getName()
+    {
+        return name;
+    }
+    public ActionPendingList getActionPendlingList()
+    {
+        return pendingList;
+    }
+    public void setActionPendingList(ActionPendingList pendingList) // reserved to ActionPendingList class
+    {
+        this.pendingList = pendingList;
+    }
+    public bool execute(float deltaTime)
+    {
+        if (!isInit)
+        {
+            isInit = true;
+            return onStart(deltaTime);
+        }
+        else if (!isEnabled)
+        {
+            isEnabled = true;
+            return onResume(deltaTime);
+        }
+        else
+            return onUpdate(deltaTime);
+    }
+    public void pause()
+    {
+        isEnabled = false;
+        onPause();
+    }
+    public void remove()
+    {
+        if (pendingList == null)
+            return;
+
+        pendingList.removeAction(this);
+        onRemove();
+    }
+    public abstract float getPriority();
+    protected abstract bool onStart(float deltaTime);
+    protected abstract bool onUpdate(float deltaTime);
+    protected abstract void onRemove();
+    protected abstract void onPause();
+    protected abstract bool onResume(float deltaTime);
+}
+class ActionComparer : IComparer<Action>
+{
+    public int Compare(Action x, Action y)
+    {
+        if (x.getPriority() > y.getPriority())
+            return 1;
+        else if (x.getPriority() < y.getPriority())
+            return -1;
+        return 0;
+    }
+}
---
+++ Assets/Classes/Actions/ActionPendingList.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ActionPendingList 
+{
+    private static string DEBUG_TAG = "[ActionPendingList]";
+    private List<Action> actions = new List<Action>();
+    private bool debug;
+    private ActionComparer actionComparer = new ActionComparer();
+
+    public void addAction(Action action)
+    {
+        if (debug)
+            Debug.Log(DEBUG_TAG+" Adding new Action : " + action.getName());
+        actions.Add(action);
+        action.setActionPendingList(this);
+    }
+    
+    public void removeAction(Action action)
+    {
+      
+        if (actions.Remove(action))
+        {
+            if (debug)
+                Debug.Log(DEBUG_TAG+" Removing Action : " + action.getName());
+            action.remove();
+        }
+    }
+    public bool execute(float deltaTime)
+    {
+        if (debug && actions.Count == 0)
+            Debug.Log(DEBUG_TAG+" Executing : Nothing to execute (empty)");
+        bool result = false;
+        while (actions.Count > 0 && !result)
+        {
+            sortActions();
+            if (debug)
+                Debug.Log(DEBUG_TAG + " Executing : " + getActualAction().getName());
+            result = actions[0].execute(deltaTime);
+            if (debug)
+                Debug.Log(DEBUG_TAG + " The action didn't consumed the token"+(actions.Count > 0 ? ", looping for execute the next action" : " and the pending list is now empty"));
+        }
+        return result;
+    }
+    private void sortActions()
+    {
+        actions.Sort(actionComparer);
+    }
+    public void setDebug(bool debug)
+    {
+        this.debug = debug;
+    }
+    public int size()
+    {
+        return actions.Count;
+    }
+    public Action getActualAction()
+    {
+        if (actions.Count > 0)
+            return actions[0];
+        return null;
+    }
+}
