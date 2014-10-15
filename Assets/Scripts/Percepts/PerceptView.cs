557df828f05a7f363ff7389aa30b1a59b671bc68
plombeur <hougmickael@gmail.com>
2014-10-15 18:12:49 +0200

    ActionPendingList debug global ajouté, test d'angle du percept view

--- Assets/Scripts/Percepts/PerceptView.cs
+++ Assets/Scripts/Percepts/PerceptView.cs
@@ -4,20 +4,15 @@ using System.Collections.Generic;
 
 public class PerceptView : MonoBehaviorAdapter 
 {
-    public float angle;
+    public float angle = 180;
     private List<Living> livings;
-
-    protected override void Update()
-    {
-        base.Update();
-
-        
-    }
 
     protected override void OnTriggerEnter2D(Collider2D collider)
     {
         base.OnTriggerEnter2D(collider);
-
+        float angle = Quaternion.Angle(collider.transform.rotation,transform.rotation);
+        if (angle > (this.angle / 2.0) || angle < -(this.angle / 2.0))
+            return;
         Living living = collider.gameObject.GetComponent<Living>();
         if (living != null)
             livings.Add(living);
