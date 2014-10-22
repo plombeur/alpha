5bf7230ab6322f2c4df162554c39964642810bf1
plombeur <hougmickael@gmail.com>
2014-10-22 17:10:55 +0200

    Detector et PerceptView operationnel

--- Assets/Scripts/Percepts/Detector.cs
+++ Assets/Scripts/Percepts/Detector.cs
@@ -11,81 +11,46 @@ public class Detector : MonoBehaviourAdapter
     private List<GameObject> objectsToAdd;
     private List<GameObject> objectsToRemove;
 
-	void Start ()
+    protected override void Start()
     {
         objectsToAdd = new List<GameObject>();
         objectsToRemove = new List<GameObject>();
         objectsInTemp = new List<GameObject>();
         objectsOutTemp = new List<GameObject>();
-	}
-	
-	void FixedUpdate ()
-    {
-        objectsToAdd.Clear();
-        objectsToRemove.Clear();
-	}
+    }
+
+
 
     protected override void OnTriggerEnter2D(Collider2D collider)
     {
         base.OnTriggerEnter2D(collider);
         if (this.angle < 360)
         {
-          
             float angle = Vector3.Angle(transform.up, collider.gameObject.transform.position - transform.position);
-           
             if (angle <= (this.angle / 2.0))
             {
-                if (objectsInTemp.Contains(collider.gameObject)) 
-                objectsToAdd.Add(collider.gameObject);
-                objectsInTemp.Add(collider.gameObject);
+                if (!objectsInTemp.Contains(collider.gameObject) && !objectsOutTemp.Contains(collider.gameObject))
+                {
+                    objectsToAdd.Add(collider.gameObject);
+                    objectsInTemp.Add(collider.gameObject);
+                }
             }
             else
             {
-                objectsOutTemp.Add(collider.gameObject);
-               
+                if (!objectsInTemp.Contains(collider.gameObject) && !objectsOutTemp.Contains(collider.gameObject))
+                    objectsOutTemp.Add(collider.gameObject);
             }
         }
         else
         {
-            objectsToAdd.Add(collider.gameObject);
-            objectsInTemp.Add(collider.gameObject);
-        }        
-    }
-    protected override void Update()
-    {
-        
-        base.Update();
-        int index = 0;
-        while (index < objectsInTemp.Count)
-        {
-            
-            GameObject o = objectsInTemp[index];
-              float angle = Vector3.Angle(transform.up, o.transform.position - transform.position);
-              if (angle > (this.angle / 2.0))
-              {
-                 
-                  objectsInTemp.Remove(o);
-                  objectsToRemove.Add(o);
-                  objectsOutTemp.Add(o);
-              }
-              ++index;
-        }
-        index = 0;
-        while (index < objectsOutTemp.Count)
-        {
-            
-            GameObject o = objectsOutTemp[index];
-            float angle = Vector3.Angle(transform.up, o.transform.position - transform.position);
-            if (angle <= (this.angle / 2.0))
+            if (!objectsInTemp.Contains(collider.gameObject) && !objectsOutTemp.Contains(collider.gameObject))
             {
-                
-                objectsOutTemp.Remove(o);
-                objectsInTemp.Add(o);
-                objectsToAdd.Add(o);
+                objectsToAdd.Add(collider.gameObject);
+                objectsInTemp.Add(collider.gameObject);
             }
-            ++index;
         }
     }
+
     protected override void OnTriggerExit2D(Collider2D collider)
     {
         base.OnTriggerExit2D(collider);
@@ -95,7 +60,48 @@ public class Detector : MonoBehaviourAdapter
             objectsToRemove.Add(collider.gameObject);
         }
     }
+    protected override void Update()
+    {
+        base.Update();
+        int index = 0;
 
+        while (index < objectsInTemp.Count)
+        {
+            bool removed = false;
+            GameObject o = objectsInTemp[index];
+            float angle = Vector3.Angle(transform.up, o.transform.position - transform.position);
+            if (angle > (this.angle / 2.0))
+            {
+                removed = objectsInTemp.Remove(o);
+                objectsToRemove.Add(o);
+                objectsOutTemp.Add(o);
+            }
+            if (!removed)
+                ++index;
+        }
+        index = 0;
+        while (index < objectsOutTemp.Count)
+        {
+            bool removed = false;
+            GameObject o = objectsOutTemp[index];
+            float angle = Vector3.Angle(transform.up, o.transform.position - transform.position);
+            if (angle <= (this.angle / 2.0))
+            {
+                removed = objectsOutTemp.Remove(o);
+                objectsInTemp.Add(o);
+                objectsToAdd.Add(o);
+            }
+            if (!removed)
+                ++index;
+        }
+    }
+    protected override void LateUpdate()
+    {
+        base.LateUpdate();
+       
+        objectsToAdd.Clear();
+        objectsToRemove.Clear();
+    }
     public List<GameObject> getEnteringGameObjets()
     {
         return objectsToAdd;
--- Assets/Scripts/Percepts/PerceptView.cs
+++ Assets/Scripts/Percepts/PerceptView.cs
@@ -8,42 +8,41 @@ public class PerceptView : MonoBehaviourAdapter
     public Detector longRangeDetector;
     private List<Living> livings, livingsShortRange, livingLongRange;
 
-    void Start()
+
+    protected override void Start()
     {
+        base.Start();
         livings = new List<Living>();
         livingsShortRange = new List<Living>();
         livingLongRange = new List<Living>();
     }
 
-    void Update()
+    protected override void Update()
     {
+        base.Update();
         foreach (GameObject objetSeen in shortRangeDetector.getEnteringGameObjets())
         {
             Living living = objetSeen.GetComponent<Living>();
             if (living != null)
                 livingsShortRange.Add(living);
-            Debug.Log("add short");
         }
         foreach (GameObject objetSeen in longRangeDetector.getEnteringGameObjets())
         {
             Living living = objetSeen.GetComponent<Living>();
             if (living != null)
                 livingLongRange.Add(living);
-            Debug.Log("add long");
         }
         foreach (GameObject objetSeen in shortRangeDetector.getExitingGameObjects())
         {
             Living living = objetSeen.GetComponent<Living>();
             if (living != null)
                 livingsShortRange.Remove(living);
-            Debug.Log("rm short");
         }
         foreach (GameObject objetSeen in longRangeDetector.getExitingGameObjects())
         {
             Living living = objetSeen.GetComponent<Living>();
             if (living != null)
                 livingLongRange.Remove(living);
-            Debug.Log("rm long");
         }
         livings.Clear();
         livings.AddRange(livingsShortRange);
@@ -55,14 +54,7 @@ public class PerceptView : MonoBehaviourAdapter
     }
 
     public List<Living> getLiving()
-    {
-       // Debug.Log("s "+livingsShortRange.Count);
-       // if (livingsShortRange.Count > 0)
-         //   Debug.Log(livingsShortRange[0]);
-       // Debug.Log("l "+livingLongRange.Count);
-        //if (livingLongRange.Count > 0)
-          //  Debug.Log(livingLongRange[0]);
-       
+    {       
         return livings;
     }
 }
