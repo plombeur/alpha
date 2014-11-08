bec1a8d9b5844181024e4e9452f504bf737e19d4
plombeur <hougmickael@gmail.com>
2014-11-08 16:09:37 +0100

    Nouveau ProgressBar directement integré dans unity

---
+++ Assets/Editor/UI/ProgressBarEditor.cs
@@ -0,0 +1,54 @@
+﻿using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+[CustomEditor(typeof(ProgressBar))]
+public class ProgressBarEditor : Editor
+{
+
+    public override void OnInspectorGUI()
+    {
+        GUI.changed = false;
+
+        ProgressBar myTarget = (ProgressBar)target;
+
+        if (myTarget.GetComponent<Image>() == null)
+            myTarget.gameObject.AddComponent<Image>().color = new Color(0.2f,0.2f,0.2f,0.3f);
+
+        if (myTarget.objectBar == null)
+        {
+            myTarget.objectBar = new GameObject("ProgressPart");
+            myTarget.objectBar.transform.parent = myTarget.transform;
+            RectTransform rectObjectBar = myTarget.objectBar.AddComponent<RectTransform>();
+            rectObjectBar.pivot = new Vector2(0, 0);
+            rectObjectBar.anchorMin = new Vector2();
+            rectObjectBar.anchorMax = new Vector2();
+            myTarget.objectBar.AddComponent<Image>();
+
+        }
+
+
+        myTarget.progress = Mathf.Clamp(EditorGUILayout.FloatField("Progress", myTarget.progress),0,100);
+        myTarget.padWidth = Mathf.Clamp(EditorGUILayout.FloatField("Pad Width", myTarget.padWidth), 0,myTarget.width/2);
+        myTarget.padHeight = Mathf.Clamp(EditorGUILayout.FloatField("Pad Height", myTarget.padHeight), 0, myTarget.height / 2);
+        myTarget.width = Mathf.Max(EditorGUILayout.FloatField("Width", myTarget.width), myTarget.padWidth * 2);
+        myTarget.height = Mathf.Max(EditorGUILayout.FloatField("Height", myTarget.height), myTarget.padHeight * 2);
+
+        RectTransform rect = myTarget.GetComponent<RectTransform>();
+        if (rect.pivot.x != 0 || rect.pivot.y != 0)
+            rect.pivot = new Vector2();
+        rect.sizeDelta = new Vector2(myTarget.width, myTarget.height);
+
+        myTarget.updateProgressBar();
+
+        if (GUI.changed)
+            EditorUtility.SetDirty(myTarget);
+    }
+    [MenuItem("GameObject/UI/ProgressBar")]
+    public static void createProgressBarObject()
+    {
+        GameObject progressBar = new GameObject("ProgressBar");
+        progressBar.AddComponent<ProgressBar>();
+    }
+}
--- Assets/Scripts/UI/ProgressBar.cs
+++ Assets/Scripts/UI/ProgressBar.cs
@@ -1,21 +1,28 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[RequireComponent(typeof(RectTransform))]
 public class ProgressBar : MonoBehaviour 
 {
     public GameObject objectBar;
-    public float progress;
-    private float originalWidth;
-
-	void Start () 
-    {
-        this.originalWidth = objectBar.GetComponent<RectTransform>().rect.width;
-	}
+    public float progress = 100;
+    public float width = 200, height = 50;
+    public float padWidth = 5, padHeight = 5;
 	
 	void Update () 
     {
-        Vector2 size = objectBar.GetComponent<RectTransform>().sizeDelta;
-        size.x = originalWidth * Mathf.Clamp(progress / 100, 0, 1);
-        objectBar.GetComponent<RectTransform>().sizeDelta = size;
+        updateProgressBar();
 	}
+
+    public void updateProgressBar()
+    {
+        RectTransform rect = objectBar.GetComponent<RectTransform>();
+
+        Vector2 size;
+        size.x = (width-(2*padWidth)) * Mathf.Clamp(progress / 100, 0, 1);
+        size.y = height - (2 * padHeight);
+        rect.sizeDelta = size;
+        rect.localPosition = new Vector2(padWidth, padHeight);
+    }
+    
 }
