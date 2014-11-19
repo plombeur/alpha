a6dcfb041f37a31f8fd6c6b6878c7e640cfa391c
Vincent <vincent.barthez.dev@gmail.com>
2014-11-19 21:52:25 +0100

    Script TollTip

---
+++ Assets/Scripts/UI/InfoTools/ToolTip.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ToolTip : MonoBehaviour {
+    public string description;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        checkTrigger();
+	}
+
+    /**
+     * Check display condition.
+     * Must be overidden.
+     * */
+    public virtual void checkTrigger()
+    {
+    }
+
+    /**
+     * Updates ToolTipManager + lock or destroy.
+     * */
+    public void display()
+    {
+        GameObject TTM = GameObject.Find("TollTipManager");
+        if (TTM != null)
+        {
+            ToolTipManager manager = TTM.GetComponent<ToolTipManager>();
+            if (manager != null)
+            {
+                manager.askDisplay(this);
+                this.enabled = false;
+            }
+            else Destroy(this);
+        }
+        else Destroy(this);
+    }
+}
---
+++ Assets/Scripts/UI/InfoTools/ToolTipManager.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ToolTipManager : MonoBehaviour {
+    public GameObject Alpha;
+    private Stack<ToolTip> mTops;
+    private ToolTip mCurrentTip;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        	
+	}
+
+    /**
+     * Add the ToolTip to the display list.
+     * */
+    public void askDisplay(ToolTip tip)
+    {
+
+    }
+    /**
+     * Freeze time.
+     * Display ToolTip.
+     * Destroy ToolTip.
+     * Check next ToolTIp to display.
+     * */
+    public void displayToolTip()
+    {
+    }
+
+    private void freezeTime()
+    {
+    }
+    private void displayToolTIp()
+    {
+    }
+    /**
+     * Called by the GUI to display the next ToolTip if existing.
+     * */
+    public void validateReading()
+    {
+    }
+}
