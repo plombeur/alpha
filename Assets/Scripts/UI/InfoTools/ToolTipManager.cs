c401fd1136b768d78b7c354e2c0adbea8e111ca8
Vincent <vincent.barthez.dev@gmail.com>
2014-11-24 10:41:50 +0100

    Update TTM and RainManager creation
    TTM:
    - Can freeze the time
    - Now has a Text

--- Assets/Scripts/UI/InfoTools/ToolTipManager.cs
+++ Assets/Scripts/UI/InfoTools/ToolTipManager.cs
@@ -1,28 +1,48 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 
-public class ToolTipManager : MonoBehaviour {
+public class ToolTipManager : MonoBehaviour
+{
     public GameObject Alpha;
-    private Stack<ToolTip> mTips;
-    private ToolTip mCurrentTip;
+    private Stack<ToolTip> m_Tips;
+    private ToolTip m_CurrentTip;
+    private Text m_Displayer;
 
-	// Use this for initialization
-	void Start () {
-        mTips = new Stack<ToolTip>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        	
-	}
+    // Use this for initialization
+    void Start()
+    {
+        m_Tips = new Stack<ToolTip>();
+        GameObject UIText = GameObject.Find("UI/ToolTipDisplayer");
+        if (UIText == null)
+        {
+            Debug.Log("TTM::Impossible de trouver UI/ToolTipDisplayer.");
+            Destroy(this);
+        }
+        else
+        {
+            m_Displayer = UIText.GetComponent<Text>();
+            if (m_Displayer == null)
+            {
+                Debug.Log("TTM::Aucun component Text trouvé.");
+                Destroy(this);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 
     /**
      * Add the ToolTip to the display list.
      * */
     public void askDisplay(ToolTip tip)
     {
-        mTips.Push(tip);
+        m_Tips.Push(tip);
     }
     /**
      * Freeze time.
@@ -32,23 +52,62 @@ public class ToolTipManager : MonoBehaviour {
      * */
     public void displayToolTip()
     {
+        if (freezeTime())
+        {
+            displayToolTipDescription();
+        }
     }
-
-    private void freezeTime()
+    /**
+     * Slow the time till pause and change the display color to smoothly appear.
+     * */
+    private bool freezeTime()
     {
+        bool isFrozen = false;
+        if (Time.timeScale == 0.0F)
+        {
+            isFrozen = true;
+        }
+        else if (Time.timeScale < 0.25F)
+        {
+            Time.timeScale = 0.0F;
+
+            Color displayColor = m_Displayer.color;
+            displayColor.a = 1.0F;
+            m_Displayer.color = displayColor;
+
+            isFrozen = true;
+        }
+        else
+        {
+            Time.timeScale = Time.timeScale / 2.0F;
+
+            Color displayColor = m_Displayer.color;
+            displayColor.a = 1 - Time.timeScale;
+            m_Displayer.color = displayColor;
+        }
+        Time.fixedDeltaTime = Time.fixedDeltaTime * Time.timeScale;
+        return isFrozen;
     }
+    /**
+     * Display the current TollTip.
+     * */
     private void displayToolTipDescription()
     {
+        m_Displayer.text = m_CurrentTip.description;
     }
     /**
      * Called by the GUI to display the next ToolTip if existing.
      * */
     public void validateReading()
     {
-        if (mCurrentTip != null)
+        if (m_CurrentTip != null)
         {
-            Destroy(mCurrentTip.gameObject);
-            mCurrentTip = mTips.Pop();
+            Destroy(m_CurrentTip.gameObject);
+            m_CurrentTip = m_Tips.Pop();
         }
+        Color baseColor = m_Displayer.color;
+        baseColor.a = 0.0F;
+        m_Displayer.color = baseColor;
+        Time.timeScale = 1.0F;
     }
 }
---
+++ Assets/Scripts/Weather/RainManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RainManager : MonoBehaviour {
+    public GameObject RainZonePrefab;
+	// Use this for initialization
+	void Start () {
+        if (RainZonePrefab == null)
+        {
+            Destroy(this.gameObject);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
