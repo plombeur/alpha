fa5c3e4bb478f867d8dc06c15977a60189593102
TRAN Quang Dung <quang-dung.tran@etud.univ-montp2.fr>
2014-11-24 19:13:32 +0100

    Début génération de map

---
+++ Assets/Scripts/Patches/Grass.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Grass : Patch
+{
+    // Use this for initialization
+    protected void Start()
+    {
+        maxNutriments = 50;
+        nutriments = maxNutriments;
+        temperature = 15;
+        passabilite = 1;
+        evaporateSpeed = 0.5f;
+    }
+
+    // Update is called once per frame
+    protected void Update()
+    {
+        base.Update();
+    }
+}
