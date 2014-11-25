737db50cdf79096c496db1fbe290846078e50602
plombeur <hougmickael@gmail.com>
2014-11-25 22:36:03 +0100

    Nouveau controlleur camera : CameraController2D
    - Fonction de suivit d'un objet target
    - Le drag de souris permet le deplacement de la camera (stop le suivit
    de la camera)
    - reactivation du suivi en cliquant sur l'icone du loup en bas a gauche
    (attention, le bouton doit etre bindé dans vos scenes, demandez moi si
    vous voulez le faire)

---
+++ Assets/Scripts/CameraController2D.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController2D : MonoBehaviour
+{
+    public bool followTarget = true;
+    public Transform target;
+    private bool mouseDown = false;
+    private Vector3 lastMousePositonOnWorld;
+    private Plane groundPlane = new Plane(-Vector3.forward, Vector3.zero);
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            Ray mouseRay = camera.ScreenPointToRay(Input.mousePosition);
+            float distanceCast;
+            groundPlane.Raycast(mouseRay, out distanceCast);
+            Vector3 worldPosition = mouseRay.GetPoint(distanceCast);
+            if (!mouseDown)
+                mouseDown = true;
+            else
+            {
+                followTarget = false;
+                Vector3 delta = worldPosition - lastMousePositonOnWorld;
+                if (delta.magnitude != 0)
+                    camera.transform.Translate(-delta);
+            }
+            mouseRay = camera.ScreenPointToRay(Input.mousePosition);
+            groundPlane.Raycast(mouseRay, out distanceCast);
+            worldPosition = mouseRay.GetPoint(distanceCast);
+            lastMousePositonOnWorld = worldPosition;
+        }
+        else
+            mouseDown = false;
+    }
+    void LateUpdate()
+    {
+        if (followTarget && target != null)
+        {
+            Vector3 position = target.position;
+            position.z = -1;
+            transform.position = position;
+        }
+    }
+
+    public void setFollowTarget(bool value)
+    {
+        followTarget = value;
+    }
+}
