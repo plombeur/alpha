69fdfbe47a1e5e5494d66236d80a6301a07396c5
TRAN Quang Dung <quang-dung.tran@etud.univ-montp2.fr>
2014-12-03 17:24:42 +0100

    Avancement de tooltips

---
+++ Assets/Scripts/UI/InfoTools/ToolTips/TipPreyDead.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TipPreyDead : ToolTip
+{
+    void Start()
+    {
+
+    }
+    protected override void checkTrigger()
+    {
+        bool isDead = false;
+        Loup alpha = mManager.Alpha.GetComponent<Loup>();
+        List<Living> percepts = alpha.perceptView.getLiving();
+        for (int i = 0; i < percepts.Count; ++i)
+            if (percepts[i] as Animal != null && percepts[i] as Loup == null && (percepts[i] as Animal).estMort())
+            {
+                isDead = true;
+            }
+
+        if (isDead) display();
+    }
+
+    protected override void checkMemoryModificationTrigger(MemoryBloc bloc)
+    {
+
+    }
+}
---
+++ Assets/Scripts/UI/InfoTools/ToolTips/TipWolfDead.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TipWolfDead : ToolTip
+{
+
+    protected override void checkTrigger()
+    {
+        bool isDead = false;
+        Loup alpha = mManager.Alpha.GetComponent<Loup>();
+        List<Living> percepts = alpha.perceptView.getLiving();
+        for (int i = 0; i < percepts.Count; ++i)
+            if (percepts[i] as Loup != null && (percepts[i] as Loup).estMort())
+            {
+                isDead = true;
+            }
+        if (isDead) display();
+    }
+
+    protected override void checkMemoryModificationTrigger(MemoryBloc bloc)
+    {
+
+    }
+}
