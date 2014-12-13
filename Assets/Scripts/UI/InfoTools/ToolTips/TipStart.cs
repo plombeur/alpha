2b682965e9edaa0fad9df5d52ecab163a52342aa
TRAN Quang Dung <quang-dung.tran@etud.univ-montp2.fr>
2014-12-13 15:19:36 +0100

    Tip start

---
+++ Assets/Scripts/UI/InfoTools/ToolTips/TipStart.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TipStart : ToolTip {
+
+    protected override void checkTrigger()
+    {
+        display();
+    }
+
+    protected override void checkMemoryModificationTrigger(MemoryBloc bloc)
+    {
+
+    }
+}
