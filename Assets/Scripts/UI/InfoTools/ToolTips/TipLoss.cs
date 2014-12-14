a525bf7b9d6118cdfaa0493543f1d8cd945b2e56
TRAN Quang Dung <quang-dung.tran@etud.univ-montp2.fr>
2014-12-14 17:48:17 +0100

    ToolTips : Loss
    Affiché quand le loup alpha est mort

---
+++ Assets/Scripts/UI/InfoTools/ToolTips/TipLoss.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TipLoss : ToolTip
+{
+
+    void Start()
+    {
+        title = " Vous avez perdu !";
+        description = " Le loup alpha est mort de faim, ou le loup beta l'a tué pour prendre sa place après avoir douté sa position...";
+    }
+
+    protected override void checkTrigger()
+    {
+        if (mManager.Alpha.GetComponent<LoupAlpha>().estMort()) display();
+    }
+
+    protected override void checkMemoryModificationTrigger(MemoryBloc bloc)
+    {
+
+    }
+}
