954b75dc415d4ac96fcfb5685e78d1aa49897762
Vincent <vincent.barthez.dev@gmail.com>
2014-12-14 15:41:22 +0100

    Sans importance
    Changement de caractère d'un script.

--- Assets/Scripts/UI/InfoTools/ToolTips/TipStart.cs
+++ Assets/Scripts/UI/InfoTools/ToolTips/TipStart.cs
@@ -5,7 +5,7 @@ public class TipStart : ToolTip {
 
     void Start()
     {
-        title = "Bienvenue sur Alpha!";
+        title = "Bienvenue sur Alpha !";
         description = "Vous prenez la place d’un loup alpha. Choisi par votre meute sur votre force, votre intelligence, votre sagesse, votre reconnaissance…,  vous devez donner des directions pour la faire survivre. L'extension de la territoire, la recherche de la nourriture, la protection des petits … vous défient tout au long du jeu. Utilisez votre connaissances et prouvez que vous êtes l'alpha!";
     }
     protected override void checkTrigger()
