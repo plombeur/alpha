c3e62d0aec6ee84120bb1f1f68609b106bfbbe6f
Vincent <vincent.barthez.dev@gmail.com>
2014-12-15 23:19:34 +0100

    Fin du tuto + tips

---
+++ Assets/Scripts/Tutorial/Objectifs/EndTutorial.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EndTutorial : Objectif {
+    public ToolTip TipEnd;
+
+    // Use this for initialization
+    void Start()
+    {
+        base.Start();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        checkAchievement();
+    }
+
+    /**
+     * Check display condition on update.
+     * Must be overidden.
+     * */
+    protected override void checkAchievement()
+    {
+    }
+
+    public override void activate()
+    {
+        //Debug.Log("Activate fils");
+        base.activate();
+        TipEnd.enabled = true;
+        achieve();
+    }
+}
---
+++ Assets/Scripts/UI/InfoTools/ToolTips/TipEndTutorial.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TipEndTutorial : ToolTip {
+    // Use this for initialization
+    void Awake()
+    {
+        isActivatedElsewhere = true;
+        title = "Fin du tutoriel.";
+        description = "Félicitation ! Tu as fini le tutoriel d'Alpha !\n\nTu peux maintenant essayer de faire survivre ta meute par toi même !\nPense à lire les informations qui te seront fournies, elles te seront utiles lorsque tu découvriras de nouvelles espèces.\n\nBon courage !\n\n~L'équipe d'Alpha.";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        checkTrigger();
+    }
+
+    protected override void checkTrigger()
+    {
+        display();
+    }
+}
